1ab348b2b30f4413c1f6eb17df4b60c5a7c75d58
Leander Parton <blparton@student.matnat.uio.no>
2022-12-18 20:31:00 +0100

    Cleanup

--- Additional Exercise/biasvariance_nn.py
+++ Additional Exercise/biasvariance_nn.py
@@ -2,42 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import data_generation as dg
 import regression_tools as rt
-from sklearn import tree
+# Import neural network from scikit-learn
+from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_wine as wine
 
 
 
+# Generate and split data
 x, y, z, _ = dg.generate_data_Franke(500, 0.1, seed=1)
 X = np.array([[x[i], y[i]] for i in range(len(x))])
 X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.25)
 
 
-
-
-
-max_depth_range = 15
+# Set up arrays for plotting
+max_node_range = 50
 bias_arr = []
 variance_arr = []
 error_arr = []
-print(f"Progress: {0} %", end="\r")
+progress = 0
 x_vals = []
+print(f"Progress: {progress} %", end="\r")
 
-for i in range(1, max_depth_range):
+
+# Loop over different number of nodes in hidden layer
+for i in range(1, max_node_range, 2):
     x_vals.append(i)
-    #print("---------------Node size: ", i, "------------------")
-    clf = tree.DecisionTreeRegressor(max_depth=i)
-    bias, variance, error = rt.bootstrap_tree(X_train, X_test, z_train, z_test, clf, 100)
+    clf = MLPRegressor(hidden_layer_sizes=(i,), activation='logistic', solver='lbfgs', max_iter=2000)
+    bias, variance, error = rt.bootstrap_nn(X_train, X_test, z_train, z_test, clf, 100)
     bias_arr.append(bias)
     variance_arr.append(variance)
     error_arr.append(error)
-    print(f"Progress: {int(i/max_depth_range*100)} %", end="\r")
+    progress = int(i/max_node_range*100)
+    print(f"Progress: {progress} %", end="\r")
 
 
+# Plot results
+plt.ylim(0, 0.05)
 plt.plot(x_vals, bias_arr, "--o", label='Bias^2', color='blue')
 plt.plot(x_vals, variance_arr, "--o", label='Variance', color='orange')
 plt.plot(x_vals, error_arr, "--o", label='Error', color='green')
-plt.title("Bias-variance tradeoff for decision tree regressor")
+plt.title("Bias-variance tradeoff for neural network")
 plt.xlabel('Number of nodes in hidden layer')
 plt.ylabel('Error scores')
 plt.legend()
